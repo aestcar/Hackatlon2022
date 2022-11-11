cbdde9b94e43290b5118388683dc8f83885f8824
Lero06 <57032316+Lero06@users.noreply.github.com>
2022-11-11 15:58:59 +0100

    mapa subido y fetch mas menos

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  fetch('https://valencia.opendatasoft.com/api/records/1.0/search/?dataset=itinerarios-ciclistas-itineraris-ciclistes&q=')
+  .then(response => response.json())
+  .then(data => alert(data));
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
