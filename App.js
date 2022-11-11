361f7d2825e5bca25fb569e3143e7e2050fcc54e
Lero06 <57032316+Lero06@users.noreply.github.com>
2022-11-11 20:04:40 +0100

    mapa con rutas

--- App.js
+++ App.js
@@ -1,17 +1,62 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Geojson, PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { kml } from "@tmcw/togeojson";
+import { DOMParser } from "xmldom";
 
 export default function App() {
-  fetch('https://valencia.opendatasoft.com/api/records/1.0/search/?dataset=itinerarios-ciclistas-itineraris-ciclistes&q=')
-  .then(response => response.json())
-  .then(data => alert(data));
+  const [myPlace, setMyPlace] = useState();
 
+  /* fetch('https://valencia.opendatasoft.com/api/records/1.0/search/?dataset=itinerarios-ciclistas-itineraris-ciclistes&q=')
+  .then(response => response.json())
+  .then(data => alert(data));*/
+
+  const fetchKML = async () => {
+    const url =
+      "https://gist.githubusercontent.com/aestcar/d60d60a351d0ab99b6d9cef9422b9345/raw/03866dba2022d745661227fcd154d323487d7a90/itinerarios-ciclistas-itineraris-ciclistes.kml";
+    const result = await axios
+      .get(url)
+      .then((res) => res.data)
+      .catch((e) => {
+        console.log(e);
+        return null;
+      });
+
+    if (result != null) {
+      const theKML = new DOMParser().parseFromString(result);
+      const converted = kml(theKML);
+      setMyPlace(converted);
+    }
+  };
+
+  useEffect(() => {
+    fetchKML();
+  }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.container}>
-        <MapView style={styles.map} />
+        {myPlace && (
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            region={{
+              latitude: 39.466667,
+              longitude: -0.375,
+              latitudeDelta:0.5,
+              longitudeDelta:0.5
+            }}
+          >
+            <Geojson
+              geojson={myPlace}
+              strokeColor="red"
+              fillColor="blue"
+              strokeWidth={3}
+            ></Geojson>
+          </MapView>
+        )}
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,7 +70,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
