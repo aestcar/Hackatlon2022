7ab8e137fd9bc68aaabd73aa7978098e46f3aec0
Joan Matarredona <53258847+zoni7@users.noreply.github.com>
2022-11-16 11:36:24 +0100

    Revert "query arreglada"
    This reverts commit 19a725ab544c682bbc0637b6a7aac4221c5c2564.

--- App.js
+++ App.js
@@ -1,18 +1,87 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import WheatherScreen from "./screens/WeatherScreen.js";
-import Map from "./screens/Map.js";
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions, TextInput } from "react-native";
+import MapView, { Geojson, PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { kml } from "@tmcw/togeojson";
+import { DOMParser } from "xmldom";
 
 export default function App() {
+  const [myPlace, setMyPlace] = useState();
+
+  const fetchKML = async () => {
+    const url =
+      "https://gist.githubusercontent.com/aestcar/d60d60a351d0ab99b6d9cef9422b9345/raw/03866dba2022d745661227fcd154d323487d7a90/itinerarios-ciclistas-itineraris-ciclistes.kml";
+    //"https://gist.githubusercontent.com/aestcar/7314e19dcfb8ea957bd9a94b05af60e2/raw/e49e1c69b735484c57b51b021319abbf1acd131c/rutas%2520en%2520bicicleta%2520por%2520la%2520ciudad%2520de%2520Valencia.kml" // URL RUTAS
+    const result = await axios
+      .get(url)
+      .then((res) => res.data)
+      .catch((e) => {
+        console.log(e);
+        return null;
+      });
+
+    if (result != null) {
+      const theKML = new DOMParser().parseFromString(result);
+      const converted = kml(theKML);
+      setMyPlace(converted);
+    }
+  };
+
+  useEffect(() => {
+    fetchKML();
+  }, []);
+
+  const [text, onChangeText] = React.useState("");
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Weather">
-        <Stack.Screen name="Weather" component={WheatherScreen} />
-        <Stack.Screen name="Map" component={Map} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <View style={styles.container}>
+        {myPlace && (
+          <View>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeText}
+              value={text}
+              placeholder="Buscar Rutas"
+            />
+            <MapView
+              style={styles.map}
+              provider={PROVIDER_GOOGLE}
+              region={{
+                latitude: 39.466667,
+                longitude: -0.375,
+                latitudeDelta: 1,
+                longitudeDelta: 1,
+              }}
+            >
+              <Geojson
+                geojson={myPlace}
+                strokeColor="red"
+                fillColor="blue"
+                strokeWidth={3}
+              ></Geojson>
+            </MapView>
+          </View>
+        )}
+      </View>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: Dimensions.get("window").width,
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: (Dimensions.get("window").height / 5) * 4,
+  },
+});
--- screens/WeatherScreen.js
+++ screens/WeatherScreen.js
@@ -2,9 +2,12 @@ import { useState, useEffect, useCallback } from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import bad_day from "../images/bad_day.gif";
 import good_day from "../images/good_day.gif";
-import { NavigationContainer } from "@react-navigation/native";
 
-export default function WheatherScreen({ navigation }) {
+const wait = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
+export default function weatherForecast() {
   const [data, setData] = useState();
   const [image, setImage] = useState();
 
@@ -33,17 +36,19 @@ export default function WheatherScreen({ navigation }) {
   const isAGoodDay = useCallback(() => {
     if (data == undefined) return;
     // Get variables
+    const stateSky = data.stateSky;
     const tempMin = data.temperaturas.min;
     const tempMax = data.temperaturas.max;
     const wind = data.viento;
     const rainProbability = data.lluvia;
 
     // def metrics
+    const validStateSky = ["Despejado", "Nubes altas"].includes(stateSky);
     const validTemp = tempMax < 40 && tempMin > 0;
     const validWind = wind < 40;
-    const validRain = rainProbability <= 50;
+    const validRain = rainProbability < 50;
     // big condition
-    if (validTemp && validWind && validRain) {
+    if (validStateSky && validTemp && validWind && validRain) {
       setImage(good_day);
     } else setImage(bad_day);
   });
@@ -52,10 +57,7 @@ export default function WheatherScreen({ navigation }) {
     <View style={styles.container}>
       <Image style={styles.image} source={image}></Image>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={navigation.navigate("Map")}
-      >
+      <TouchableOpacity style={styles.button}>
         <Text style={{ fontSize: 20, color: "#ffffff", margin: 4 }}>
           {" "}
           CONTINUAR{" "}
