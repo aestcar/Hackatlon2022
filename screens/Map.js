7c3f843b2b09758abb0eb6703276a422ed817898
Joan Matarredona <53258847+zoni7@users.noreply.github.com>
2022-11-17 19:57:47 +0100

    rectangulos de mapa

--- screens/Map.js
+++ screens/Map.js
@@ -19,32 +19,33 @@ export default function Map({ navigation }) {
 
   const fetchKML = async () => {
     let url;
-    if(myReservasOn == false){
+    if (myReservasOn == false) {
       url =
         "https://gist.githubusercontent.com/aestcar/d60d60a351d0ab99b6d9cef9422b9345/raw/03866dba2022d745661227fcd154d323487d7a90/itinerarios-ciclistas-itineraris-ciclistes.kml";
-    }else{
+    } else {
       url =
-      "https://gist.githubusercontent.com/aestcar/7314e19dcfb8ea957bd9a94b05af60e2/raw/e49e1c69b735484c57b51b021319abbf1acd131c/rutas%2520en%2520bicicleta%2520por%2520la%2520ciudad%2520de%2520Valencia.kml"; // URL RUTAS
+        "https://gist.githubusercontent.com/aestcar/7314e19dcfb8ea957bd9a94b05af60e2/raw/e49e1c69b735484c57b51b021319abbf1acd131c/rutas%2520en%2520bicicleta%2520por%2520la%2520ciudad%2520de%2520Valencia.kml"; // URL RUTAS
     }
-      const result = await axios
-        .get(url)
-        .then((res) => res.data)
-        .catch((e) => {
-          return null;
-        });
+    const result = await axios
+      .get(url)
+      .then((res) => res.data)
+      .catch((e) => {
+        return null;
+      });
 
-
-
-      if (result != null) {
-        const theKML = new DOMParser().parseFromString(result);
-        const converted = kml(theKML);
-        setMyPlace(converted);
-      }
+    if (result != null) {
+      const theKML = new DOMParser().parseFromString(result);
+      const converted = kml(theKML);
+      setMyPlace(converted);
+    }
   };
 
   const cambiarAMisReservas = () => {
-    if(myReservasOn == true){myReservasOn = false;}
-    else{myReservasOn = true}
+    if (myReservasOn == true) {
+      myReservasOn = false;
+    } else {
+      myReservasOn = true;
+    }
   };
 
   useEffect(() => {
@@ -62,7 +63,7 @@ export default function Map({ navigation }) {
               style={styles.input}
               onChangeText={onChangeText}
               value={text}
-              placeholder="Buscar Rutas"
+              placeholder=""
             />
             <MapView
               style={styles.map}
@@ -127,6 +128,6 @@ const styles = StyleSheet.create({
     left: 10,
     zIndex: 5,
     display: "flex",
-    flexDirection:"row"
+    flexDirection: "row",
   },
 });
